38ed8603831fd4b9a71f3e8dc8487f81352daf83
CoryRodriguez <mrcoryrodriguez@gmail.com>
2018-01-17 23:26:17 -0600

    fixed file structure

---
+++ src/js/reveal.js
@@ -0,0 +1,105 @@
+window.sr = ScrollReveal();
+		sr.reveal('.anim1', {
+			container: '.container',
+			viewFactor: 0,
+			useDelay: 'onload',
+			delay: 0,
+			mobile: true
+		});
+		sr.reveal('.navbar-toggler', {
+			container: '.container',
+			delay: 0,
+			origin: 'top',
+			mobile: true,
+		});
+		// sr.reveal('.anim2', {
+		// 	container: '.container',
+		// 	delay: 100,
+		// 	origin: 'top',
+		// 	mobile: true,
+		// });
+		// sr.reveal('.anim3', {
+		// 	container: '.container',
+		// 	delay: 200,
+		// 	origin: 'top',
+		// 	mobile: true
+		// });
+		// sr.reveal('.anim4', {
+		// 	container: '.container',
+		// 	delay: 300,
+		// 	origin: 'top',
+		// 	mobile: true
+		// });
+		// sr.reveal('.anim5', {
+		// 	container: '.container',
+		// 	delay: 400,
+		// 	origin: 'top',
+		// 	mobile: true
+		// });
+		sr.reveal('.anim6', {
+			container: '.container',
+			delay: 500,
+			distance: '500px',
+			origin: 'bottom',
+			mobile: true
+		});
+		sr.reveal('.skills', {
+			delay: 50,
+			distance: '500px',
+			origin: 'bottom',
+			mobile: true
+		});
+		sr.reveal('.vision', {
+			delay: 300,
+			duration: 1000,
+			distance: '250px',
+			origin: 'bottom',
+			mobile: true,
+			viewFactor: 0.3
+		});
+		sr.reveal('.ux', {
+			delay: 300,
+			duration: 1000,
+			distance: '250px',
+			origin: 'top',
+			mobile: true,
+			viewFactor: 0.3
+		});
+		sr.reveal('.dev', {
+			delay: 300,
+			duration: 1000,
+			distance: '250px',
+			origin: 'bottom',
+			mobile: true,
+			viewFactor: 0.3
+		});
+		sr.reveal('.enter-left', {
+			delay: 500,
+			duration: 1000,
+			distance: '200px',
+			origin: 'left',
+			mobile: true,
+		});sr.reveal('.enter-right', {
+			delay: 500,
+			duration: 1000,
+			distance: '200px',
+			origin: 'right',
+			mobile: true,
+		});
+		sr.reveal('.enter-left', {
+			delay: 500,
+			duration: 1000,
+			distance: '200px',
+			origin: 'left',
+			mobile: true,
+			viewFactor: 0.3
+		});sr.reveal('.enter-right', {
+			delay: 500,
+			duration: 1000,
+			distance: '200px',
+			origin: 'right',
+			mobile: true,
+			viewFactor: 0.3
+		});
+
+		
